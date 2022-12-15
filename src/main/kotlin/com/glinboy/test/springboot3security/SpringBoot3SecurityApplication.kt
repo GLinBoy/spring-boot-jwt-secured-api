dc8d0e123c35f493a91b153a082cf30f00c73790
GLinBoy <hojjatabedie@gmail.com>
2022-12-15 13:51:49 +0100

    🕯 START: Initiated the project
    resolved #1

---
+++ src/main/kotlin/com/glinboy/test/springboot3security/SpringBoot3SecurityApplication.kt
@@ -0,0 +1,11 @@
+package com.glinboy.test.springboot3security
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBoot3SecurityApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBoot3SecurityApplication>(*args)
+}
---
+++ src/test/kotlin/com/glinboy/test/springboot3security/SpringBoot3SecurityApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glinboy.test.springboot3security
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBoot3SecurityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

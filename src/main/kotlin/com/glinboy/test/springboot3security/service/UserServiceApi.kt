3bef571fb5e9cceac90f1585b9d7bec39dc7056e
GLinBoy <hojjatabedie@gmail.com>
2023-01-06 16:54:42 +0100

    👌 IMPROVE: Added CRUD method to User service API
    #24

--- src/main/kotlin/com/glinboy/test/springboot3security/service/UserServiceApi.kt
+++ src/main/kotlin/com/glinboy/test/springboot3security/service/UserServiceApi.kt
@@ -1,8 +1,15 @@
 package com.glinboy.test.springboot3security.service
 
 import com.glinboy.test.springboot3security.entity.User
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.security.core.userdetails.UserDetailsService
+import java.util.*
 
 interface UserServiceApi: UserDetailsService {
+    fun getUsers(pageable: Pageable): Page<User>
+    fun getUser(id: String): Optional<User>
+    fun saveUser(book: User): User
+    fun deleteUser(id: String)
     fun register(user: User)
 }

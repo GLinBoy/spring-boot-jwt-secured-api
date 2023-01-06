806b96a2312d4d88a2add3981e5fe417bd94237f
GLinBoy <hojjatabedie@gmail.com>
2023-01-06 16:55:08 +0100

    👌 IMPROVE: Added implementation of CRUD methods to User service Impl
    #24

--- src/main/kotlin/com/glinboy/test/springboot3security/service/impl/UserServiceApiImpl.kt
+++ src/main/kotlin/com/glinboy/test/springboot3security/service/impl/UserServiceApiImpl.kt
@@ -4,24 +4,34 @@ import com.glinboy.test.springboot3security.entity.User
 import com.glinboy.test.springboot3security.repository.UserRepository
 import com.glinboy.test.springboot3security.service.UserServiceApi
 import com.glinboy.test.springboot3security.service.dto.UserSecurity
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
+import java.util.*
 
 @Service
 class UserServiceImpl(private val repository: UserRepository): UserServiceApi {
     override fun register(user: User) {
         repository.findById(user.email).ifPresent {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST,
-                "Email exist: %s".format(user.email))
+            throw ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Email exist: %s".format(user.email)
+            )
         }
-            repository.save(user)
+        repository.save(user)
     }
 
     override fun loadUserByUsername(username: String): UserDetails =
         repository.findById(username)
             .map { UserSecurity(it) }
             .orElseThrow { UsernameNotFoundException("User not found") }
+
+    override fun getUsers(pageable: Pageable): Page<User> = repository.findAll(pageable)
+    override fun getUser(id: String): Optional<User> = repository.findById(id)
+    override fun saveUser(user: User): User = repository.save(user)
+    override fun deleteUser(id: String) = repository.deleteById(id)
 }

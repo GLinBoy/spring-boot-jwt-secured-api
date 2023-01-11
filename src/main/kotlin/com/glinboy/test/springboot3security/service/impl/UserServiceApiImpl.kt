bf429dfc6f9dc4d55abbe79041284d9655a27014
GLinBoy <hojjatabedie@gmail.com>
2023-01-11 11:27:06 +0100

    👌 IMPROVE: Check user is admin on getting all user data

--- src/main/kotlin/com/glinboy/test/springboot3security/service/impl/UserServiceApiImpl.kt
+++ src/main/kotlin/com/glinboy/test/springboot3security/service/impl/UserServiceApiImpl.kt
@@ -7,6 +7,8 @@ import com.glinboy.test.springboot3security.service.dto.UserSecurity
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
@@ -30,7 +32,16 @@ class UserServiceImpl(private val repository: UserRepository): UserServiceApi {
             .map { UserSecurity(it) }
             .orElseThrow { UsernameNotFoundException("User not found") }
 
-    override fun getUsers(pageable: Pageable): Page<User> = repository.findAll(pageable)
+    override fun getUsers(pageable: Pageable): Page<User> {
+        val auth: Authentication = SecurityContextHolder.getContext().authentication
+        if (auth.authorities.stream().anyMatch { it.authority == "ROLE_ADMIN" }) {
+            return repository.findAll(pageable)
+        } else {
+            throw ResponseStatusException(HttpStatus.FORBIDDEN,
+                "You don't have permit to get information of this user")
+        }
+    }
+
     override fun getUser(id: String): Optional<User> = repository.findById(id)
     override fun saveUser(user: User): User = repository.save(user)
     override fun deleteUser(id: String) = repository.deleteById(id)
